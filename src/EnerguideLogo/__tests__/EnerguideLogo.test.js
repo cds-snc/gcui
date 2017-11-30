43c7c4c2ec85970145c2a04fb0ac9485c6be2e10
Mike Williamson <mike.williamson@tbs-sct.gc.ca>
2017-11-30 09:41:15 -0500

    Remove final traces of styled-components

--- src/EnerguideLogo/__tests__/EnerguideLogo.test.js
+++ src/EnerguideLogo/__tests__/EnerguideLogo.test.js
@@ -1,22 +1,20 @@
-import React from 'react'
-import { mount, shallow } from 'enzyme'
-import 'jest-styled-components'
-import toJSON from 'enzyme-to-json'
-import EnerguideLogo from '../../EnerguideLogo'
+import React from "react"
+import { shallow } from "enzyme"
+import EnerguideLogo from "../../EnerguideLogo"
 
-describe('<EnerguideLogo />', () => {
-  it('renders svg', () => {
+describe("<EnerguideLogo />", () => {
+  it("renders svg", () => {
     let wrapper = shallow(<EnerguideLogo />)
-    expect(wrapper.is('svg')).toBeTruthy()
+    expect(wrapper.is("svg")).toBeTruthy()
   })
 
-  it('accepts a width prop', () => {
+  it("accepts a width prop", () => {
     let wrapper = shallow(<EnerguideLogo width="1000%" />)
-    expect(wrapper.props().width).toEqual('1000%')
+    expect(wrapper.props().width).toEqual("1000%")
   })
 
-  it('defaults to width of 10em', () => {
+  it("defaults to width of 10em", () => {
     let wrapper = shallow(<EnerguideLogo />)
-    expect(wrapper.props().width).toEqual('15em')
+    expect(wrapper.props().width).toEqual("15em")
   })
 })
