fed199433b843ce3a70243406cc2587dfd946334
Mike Williamson <mike.williamson@tbs-sct.gc.ca>
2017-11-30 10:24:07 -0500

    Fix minor linting errors: unused vars & imports

--- src/UpwardChevron/__tests__/UpwardChevron.test.js
+++ src/UpwardChevron/__tests__/UpwardChevron.test.js
@@ -1,45 +1,42 @@
-import React from 'react'
-import { mount, shallow } from 'enzyme'
-import { sheet, flush } from 'emotion'
-import toJSON from 'enzyme-to-json'
-import UpwardChevron from '../../UpwardChevron'
+import React from "react"
+import { mount, shallow } from "enzyme"
+import { sheet, flush } from "emotion"
+import UpwardChevron from "../../UpwardChevron"
 
 const stringify = stylesheet =>
-  stylesheet.tags.map(tag => tag.textContent || '').join('')
+  stylesheet.tags.map(tag => tag.textContent || "").join("")
 
-describe('<UpwardChevron />', () => {
+describe("<UpwardChevron />", () => {
   afterEach(() => flush())
 
-  it('can be rendered', () => {
+  it("can be rendered", () => {
     let wrapper = shallow(<UpwardChevron />).dive()
-    expect(wrapper.is('span')).toBeTruthy()
+    expect(wrapper.is("span")).toBeTruthy()
   })
 
-  it('accepts a width prop', () => {
-    let wrapper = shallow(<UpwardChevron width="1000%" />).find('svg')
-    expect(wrapper.props().width).toEqual('1000%')
+  it("accepts a width prop", () => {
+    let wrapper = shallow(<UpwardChevron width="1000%" />).find("svg")
+    expect(wrapper.props().width).toEqual("1000%")
   })
 
-  it('defaults to width of 0.7em', () => {
-    let wrapper = shallow(<UpwardChevron />).find('svg')
-    expect(wrapper.props().width).toEqual('0.7em')
+  it("defaults to width of 0.7em", () => {
+    let wrapper = shallow(<UpwardChevron />).find("svg")
+    expect(wrapper.props().width).toEqual("0.7em")
   })
 
-  it('accepts a colour', () => {
+  it("accepts a colour", () => {
     let wrapper = shallow(<UpwardChevron colour="#abc" />)
-    let path = wrapper.find('path')
-    expect(path.props().style.fill).toEqual('#abc')
+    let path = wrapper.find("path")
+    expect(path.props().style.fill).toEqual("#abc")
   })
 
-  it('allows the padding around the chevron to be set', () => {
-    let wrapper = mount(
-      <UpwardChevron verticalPadding="10vh" horizontalPadding="4em" />,
-    )
+  it("allows the padding around the chevron to be set", () => {
+    mount(<UpwardChevron verticalPadding="10vh" horizontalPadding="4em" />)
     expect(stringify(sheet)).toMatch(/padding:10vh 4em/)
   })
 
-  it('the padding has default values of 0.3em', () => {
-    let wrapper = shallow(<UpwardChevron />).dive()
+  it("the padding has default values of 0.3em", () => {
+    shallow(<UpwardChevron />).dive()
     expect(stringify(sheet)).toMatch(/padding:0.3em 0.3em/)
   })
 })
--- src/WordMark/__tests__/WordMark.test.js
+++ src/WordMark/__tests__/WordMark.test.js
@@ -1,39 +1,36 @@
-import React from 'react'
-import { mount, shallow } from 'enzyme'
-import { sheet, flush } from 'emotion'
-import toJSON from 'enzyme-to-json'
-import WordMark from '../../WordMark'
+import React from "react"
+import { mount, shallow } from "enzyme"
+import { sheet, flush } from "emotion"
+import WordMark from "../../WordMark"
 
 const stringify = stylesheet =>
-  stylesheet.tags.map(tag => tag.textContent || '').join('')
+  stylesheet.tags.map(tag => tag.textContent || "").join("")
 
-describe('<WordMark />', () => {
+describe("<WordMark />", () => {
   afterEach(() => flush())
 
-  it('renders svg', () => {
+  it("renders svg", () => {
     let wrapper = shallow(<WordMark />)
-    expect(wrapper.is('svg')).toBeTruthy()
+    expect(wrapper.is("svg")).toBeTruthy()
   })
 
-  it('accepts a width prop', () => {
+  it("accepts a width prop", () => {
     let wrapper = shallow(<WordMark width="1000%" />)
-    expect(wrapper.props().width).toEqual('1000%')
+    expect(wrapper.props().width).toEqual("1000%")
   })
 
-  it('defaults to width of 10em', () => {
+  it("defaults to width of 10em", () => {
     let wrapper = shallow(<WordMark />)
-    expect(wrapper.props().width).toEqual('10em')
+    expect(wrapper.props().width).toEqual("10em")
   })
 
-  it('allows the flag colour to be set', () => {
-    let wrapper = mount(<WordMark flag="#fff" />)
-    let flag = wrapper.find('.fipflag')
+  it("allows the flag colour to be set", () => {
+    mount(<WordMark flag="#fff" />)
     expect(stringify(sheet)).toMatch(/fill:#fff/)
   })
 
-  it('allows the text colour to be set', () => {
-    let wrapper = mount(<WordMark text="#fff" />)
-    let text = wrapper.find('.fiptext')
-    expect(stringify(sheet)).toMatch(/fill:#fff/)
+  it("allows the text colour to be set", () => {
+    mount(<WordMark text="#ddd" />)
+    expect(stringify(sheet)).toMatch(/fill:#ddd/)
   })
 })
