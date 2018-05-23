d7702899ed7073cfff52e321cae9e8f3aa6b50b5
Mike Williamson <mike@korora.ca>
2018-05-23 16:50:45 -0400

    Refactor test setup into setupTests.js
    This slims down the tests by making use of Jest's
    setuptestframeworkscriptfile option.

--- src/Button/__tests__/Button.test.js
+++ src/Button/__tests__/Button.test.js
@@ -1,29 +1,25 @@
-import "raf/polyfill"
-import React from "react"
-import { shallow, configure } from "enzyme"
-import { sheet, flush } from "emotion"
-import Button from "../../Button"
-import Adapter from "enzyme-adapter-react-16"
-
-configure({ adapter: new Adapter() })
+import React from 'react'
+import { shallow } from 'enzyme'
+import { sheet, flush } from 'emotion'
+import Button from '../../Button'
 
 const stringify = stylesheet =>
-  stylesheet.tags.map(tag => tag.textContent || "").join("")
+  stylesheet.tags.map(tag => tag.textContent || '').join('')
 
-describe("<Button />", () => {
+describe('<Button />', () => {
   afterEach(() => flush())
 
-  it("can be rendered", () => {
+  it('can be rendered', () => {
     let wrapper = shallow(<Button />).dive()
-    expect(wrapper.is("button")).toBeTruthy()
+    expect(wrapper.is('button')).toBeTruthy()
   })
 
-  it("has a default background color", () => {
+  it('has a default background color', () => {
     shallow(<Button>submit</Button>).dive()
     expect(stringify(sheet)).toMatch(/background-color:#eaebed/)
   })
 
-  it("<Button primary /> has a different background-color", () => {
+  it('<Button primary /> has a different background-color', () => {
     shallow(<Button primary>submit</Button>).dive()
     expect(stringify(sheet)).toMatch(/background-color:#335075/)
   })
