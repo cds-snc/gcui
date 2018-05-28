cc348f0ad3721423d7fc8f3675b14f62d558223d
Mike Williamson <mike@korora.ca>
2018-05-28 13:12:21 -0400

    Add mediaquery to PhaseBadge.

---
+++ src/utils.js
@@ -0,0 +1,21 @@
+import { css } from 'react-emotion'
+
+export const breakpoints = {
+  xs: 481,
+  small: 578,
+}
+
+export const mediaQuery = Object.keys(breakpoints).reduce(
+  (accumulator, label) => {
+    let prefix = typeof breakpoints[label] === 'string' ? '' : 'max-width:'
+    let suffix = typeof breakpoints[label] === 'string' ? '' : 'px'
+    accumulator[label] = cls =>
+      css`
+        @media screen and (${prefix + breakpoints[label] + suffix}) {
+          ${cls};
+        }
+      `
+    return accumulator
+  },
+  {}
+)
